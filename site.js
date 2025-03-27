18592c5e97913ed70c983b0003469b3b90c6f2b4
unknown <davidchinski@gmail.com>
2025-03-27 20:38:17 -0300

    comenzar html y js

---
+++ site.js
@@ -0,0 +1,4 @@
+let tareaAgregada = document.getElementById('tareaAgregada');
+let btnTareaAgregada = document.getElementById('agregar-tarea-boton');
+let listaTareas = document.getElementById('listaTareas');
+let btnEliminarCompletadas = ('eliminarCompletadas');
